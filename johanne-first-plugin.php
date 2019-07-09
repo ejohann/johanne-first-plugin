4803f644c795074f68a8e5a68f2753a087a285bf
Johanne <johanne_lewis@hotmail.com>
2019-07-09 16:50:00 +0100

    Remove the edit title function

--- johanne-first-plugin.php
+++ johanne-first-plugin.php
@@ -6,26 +6,10 @@
   */
 
     add_filter('the_content', 'amazingContentEdits');
-    add_filter('the_title', 'amazingTitleEdits');
 
     function amazingContentEdits($content){
     	$content = $content . '<p><em>All content belongs to <strong>Fictional University</strong>.</em></p>';
     	return $content;
-    }
-
-
-     function amazingTitleEdits($title){
-
-     	if(get_post_type(get_the_ID()) == 'event')
-     	 {
-    	   $title = $title . ' Event';
-    	 } 
-
-    	if(get_post_type(get_the_ID()) == 'program')
-     	 {
-    	   $title = $title . ' Program';
-    	 }
-    	return $title;
     }
 
     add_shortcode('programCount', 'returnProgramCount');
