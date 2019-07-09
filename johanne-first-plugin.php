7a07bf5bf8d1ca50194a6f968ac6fac1c1e037aa
Johanne <johanne_lewis@hotmail.com>
2019-07-09 14:44:22 +0100

    Add a footer to all posts

--- johanne-first-plugin.php
+++ johanne-first-plugin.php
@@ -5,7 +5,12 @@
 
   */
 
+    add_filter('the_content', 'amazingContentEdits');
 
+    function amazingContentEdits($content){
+    	$content = $content . '<p><em>All content belongs to <strong>Fictional University</strong>.</em></>';
+    	return $content;
+    }
 
 
 
